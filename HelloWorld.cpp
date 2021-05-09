8fbb46e2e8ec7a3ca66b79dde8267879c07dc4a7
Prerit Dayal <prerit714@gmail.com>
2021-05-09 23:40:51 +0530

    second commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef	vector<int>         vi;
+typedef	vector<string>      vs;
+typedef vector<char>        vc;
+typedef	pair<int, int>      pii;
+typedef	vector<pii>         vpii;
+typedef	vector<vi>          vvi;
+typedef long long           ll;
+#define	rep(i, a, b)        for(auto i{a}; i<=(b); ++i)
+#define	repx(i, a, b, x)    for(auto i{a}; i<=(b); i+=(x))
+#define	itr(i, v)           for(auto i{v.begin()}; i!=v.end(); ++i)
+#define	ritr(i, v)          for(auto i{v.rbegin()}; i!=v.rend(); ++i)
+#define MP                  make_pair
+#define	PB                  push_back
+#define	F                   first
+#define	S                   second
+#define NL                  '\n'
+#define SQ(a)               (a)*(a)
+#define CB(a)               (a)*(a)*(a)
+#define Log(x)              cout << x << endl
+#define Err(x)              cout << x << endl
+#define SIZE(v)             (sizeof(v)/sizeof(v[0]))
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    Log("Hello World!");
+    return 0;
+}
+
