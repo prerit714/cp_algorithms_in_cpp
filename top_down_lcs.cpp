9be474f4d36216d7de5729de19946adc5e109a49
Prerit Dayal <prerit714@gmail.com>
2021-05-11 20:15:16 +0530

    commit 3

--- top_down_lcs.cpp
+++ top_down_lcs.cpp
@@ -21,25 +21,32 @@ typedef long long           ll;
 #define Log(x)              cout << x << endl
 #define Err(x)              cout << x << endl
 #define SIZE(v)             (sizeof(v)/sizeof(v[0]))
-int t[1000][1000];
-int lcs(const string& x, const string& y, size_t n, size_t m) {
-    if (n==0||m==0)
-        return 0;
-    if (t[n][m]!=-1)
-        return t[n][m];
-    if (x[n-1]==y[m-1])
-        return t[n][m] = 1+lcs(x, y, n-1, m-1);
-    return t[n][m] = max(lcs(x, y, n-1, m), lcs(x, y, n, m-1));
+int top_down_lcs(string const& x, string const& y, size_t const n, size_t const m) {
+    int t[n+1][m+1];
+    rep(i, 0, n) {
+        rep(j, 0, m) {
+            if (i==0||j==0)
+                t[i][j] = 0;
+            else
+                t[i][j] = INT_MIN;
+        }
+    }
+    rep(i, 1, n) {
+        rep(j, 1, m) {
+            if (x[i-1]==y[j-1])
+                t[i][j] = 1+t[i-1][j-1];
+            else
+                t[i][j] = max(t[i-1][j], t[i][j-1]);
+        }
+    }
+    return t[n][m];
 }
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    memset(t, -1, sizeof(t));
-    string x{"abcdeh"};
+    string x{"abcdgh"};
     string y{"abedfhr"};
-    size_t n{x.size()};
-    size_t m{y.size()};
-    int p{lcs(x, y, n, m)};
+    int p{top_down_lcs(x, y, x.size(), y.size())};
     cout << p << NL;
     return 0;
 }
